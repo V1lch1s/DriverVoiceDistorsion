9ab6038d6f4f9c713e5aa72f46c1b57ad2646794
V1lch1s <camv03@hotmail.com>
2025-01-16 13:02:32 -0600

    Actualización de comandos de compilación

--- compilator.bat
+++ compilator.bat
@@ -18,7 +18,22 @@ rem Inicializamos el entorno
 call "D:\Visual Studio\IDE\VC\Auxiliary\Build\vcvars64.bat"
 
 rem Compilamos el código fuente
-:: Usamos ^ para extender el comando a varias lineas  
+:: Desglose de los comandos necesarios:
+:: 'cl.exe': Llama al compilador de C/C++ de Microsoft Visual Studio IDE (No me gustó y me lo traje al Editor de código para potenciar el aprendizaje con un producto más "Artesanal").
+:: '/Zi': Genera información de depuración.
+:: '/W3': Establece el nivel de advertencias (/W<n> donde el predeterminado es n=1).
+:: '/WX-': Desactiva el tratamiento de advertencias como errores.
+:: '/D': Define macros para el preprocesador.
+:: 'WIN32': Es una definición de macro para el preprocesador que indica que el código se compila para la API de Windows de 32 bits.
+:: '_WDM_DRIVER_': Es una definición de macro que indica que se está escribiendo un driver basado en el modelo Windows Driver Model.
+:: '/I': Especifica directorios de inclusión para los archivos de encabezado.
+:: '<NombreDelArchivo>.c': Es el archivo fuente que deseas compilar.
+:: 'link': Indica que los siguientes parámetros son para el enlazador.
+:: '/OUT:driver.sys': Especifica el nombre del archivo de salida.
+:: '/SUBSYSTEM:NATIVE': Indica que se está creando un controlador de modo nativo.
+:: '/NODEFAULTLIB': Evita que se vinculen bibliotecas predeterminadas.
+
+:: Usamos ^ para extender el comando a varias lineas
 cl.exe /Zi /W3 /WX- /D WIN32 /D _KERNEL_MODE /D _WDM_DRIVER_ /Gz ^
 :: Incluimos todo lo necesario del Windows Software Development Kit (/km, /shared y /um)
 /I "D:\Windows Kits\10\Include\10.0.26100.0\km" ^
@@ -36,3 +51,11 @@ if %ERRORLEVEL% neq 0 (
 )
 
 :: Habilitar el modo de prueba con este comando para omitir la firma del driver: bcdedit /set testsigning on
+:: Para Firmar el Driver: signtool sign /v /s "Nombre del certificado" /tr http://timestamp.digicert.com /fd SHA256 <NombreDelDriver>.sys
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Macro en programación: es una definición utilizada para asignar nombres a valores constantes o ::
+::                        bloques de código antes de la compilación, para cambiar la forma en que ::
+::                        el compilador traduce el código fuente (de C) al binario (0 y 1 que en  ::
+::                        el notepad se interpretan como caracteres ilegibles).   ~ChatGPT        ::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
